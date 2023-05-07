be00387a263eefecf8060dda4587c0a395ae494e
ALBANO Daniel <deanidev@gmail.com>
2023-05-07 23:39:19 +0200

    deployed

--- src/pages/Pomodoro.jsx
+++ src/pages/Pomodoro.jsx
@@ -6,12 +6,10 @@ import PomodoroForm from "../components/PomodoroForm";
 import alarmSound from "../sounds/1.m4a";
 
 import { theme } from "../theme.js";
-import {  Alert, Button, Paper, Typography } from "@mui/material";
+import { Alert,  Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import PomodoroChips from "../components/PomodoroChips";
 import { ThemeProvider } from "@emotion/react";
-
-// import useSound from "use-sound"; no need ATM
 
 export default function Pomodoro() {
   const [timerSettings, setTimerSettings] = useState("");
@@ -23,14 +21,14 @@ export default function Pomodoro() {
   const { workTime, breakTime, bigPause } = timerSettings;
 
   const pomodoroStages = [
-    0.01, //  workTime, 
-    0.01, //breakTime, //
-    0.01, // workTime, //
-    0.01, //breakTime, //
-    0.01, // workTime, //
-    0.01, //breakTime, //
-    0.01, // workTime, //
-    0.02, // bigPause, //
+    workTime,
+    breakTime,
+    workTime,
+    breakTime,
+    workTime,
+    breakTime,
+    workTime,
+    bigPause,
   ];
 
   const pomodoroStagesInSeconds = pomodoroStages.map((num) => num * 60);
@@ -59,52 +57,59 @@ export default function Pomodoro() {
   return (
     <React.Fragment>
       <ThemeProvider theme={theme}>
-      <Box
-        sx={{
-          display: "flex",
-          p: 1,
-          justifyContent: "center",
-        }}
-      >
         <Box
           sx={{
-            bgcolor: "background.paper",
-            boxShadow: 1,
-            borderRadius: 2,
-            p: 5,
-            width: '400px',
-            minWidth: 300,
+            display: "flex",
+            p: 1,
+            justifyContent: "center",
           }}
         >
-          <PomodoroForm handleSettings={handleTimerSettings} toggleTimer={toggleTimer} pomodoroFinished={pomodoroFinished}/>
+          <Box
+            sx={{
+              bgcolor: "background.paper",
+              boxShadow: 1,
+              borderRadius: 2,
+              p: 5,
+              width: "400px",
+              minWidth: 300,
+            }}
+          >
+            <PomodoroForm
+              handleSettings={handleTimerSettings}
+              toggleTimer={toggleTimer}
+              pomodoroFinished={pomodoroFinished}
+            />
 
-          {toggleTimer &&
-            (reset === false ? (
-              <Box sx={{ alignContent: 'center', pt: 3}}>
-                <Box sx={{ pb: 5 }}>
-                  <Typography color="grey" paddingBottom={1}>current progress: </Typography>
-                <PomodoroChips index={index} />
+            {toggleTimer &&
+              (reset === false ? (
+                <Box sx={{ alignContent: "center", pt: 3 }}>
+                  <Box sx={{ pb: 5 }}>
+                    <Typography color="grey" paddingBottom={1}>
+                      current progress:{" "}
+                    </Typography>
+                    <PomodoroChips index={index} />
+                  </Box>
+                  <PomodoroTimer
+                    expiryTimestamp={time}
+                    onExpire={stageFinished}
+                    key={index}
+                  />
                 </Box>
-                <PomodoroTimer
-                  expiryTimestamp={time}
-                  onExpire={stageFinished}
-                  key={index}
-                />
-              </Box>
-            ) : (
-              <Box sx={{ alignContent: 'center', pt: 3}}>
-                {/* <Typography color="success" textAlign="center"></Typography> */}
-                <Alert sx={{ height: '70px', pt: 5}} onClose={() => {
-                    setReset(false);
-                    setToggleTimer(false);
-                  }}>
-                    Pomodoro Finished!!!! 
-                </Alert>
-
-              </Box>
-            ))}
+              ) : (
+                <Box sx={{ alignContent: "center", pt: 3 }}>
+                  <Alert
+                    sx={{ height: "70px", pt: 5 }}
+                    onClose={() => {
+                      setReset(false);
+                      setToggleTimer(false);
+                    }}
+                  >
+                    Pomodoro Finished!!!!
+                  </Alert>
+                </Box>
+              ))}
+          </Box>
         </Box>
-      </Box>
       </ThemeProvider>
     </React.Fragment>
   );
